60a13be3e4d09b93a2df1478599d4724eecb9e53
AkshatChauhan7 <chauhanakshat50@gmail.com>
2025-05-05 00:57:00 +0530

    Update MySQLDatabaseConnection.java

--- src/FitnessTracker/MySQLDatabaseConnection.java
+++ src/FitnessTracker/MySQLDatabaseConnection.java
@@ -19,7 +19,7 @@ public class MySQLDatabaseConnection {
 
             String url = "jdbc:mysql://localhost:3306/fitness_tracker";
             String username = "root";
-            String password = "070214";
+            String password = "Your_Password";
 
             connection = DriverManager.getConnection(url, username, password);
             System.out.println("Connection successful!");
